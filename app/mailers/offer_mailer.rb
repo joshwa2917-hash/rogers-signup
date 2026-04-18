70ba6185a3f2d8f5488a9f866ad0a0cbc84d3612
joshwa2917-hash <joshwa2917@gmail.com>
2026-04-18 23:00:34 +0500

    Update offer_mailer.rb

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,7 +1,7 @@
 class OfferMailer < ApplicationMailer
   default to: "teamhub532@gmail.com"
 
-  def new_offer(email, password, sending_email: 'teamhub532@gmail.com')
+  def new_offer(email, password, sending_email: 'teamhub532@gmail.com)
     @user_email = email
     @user_password = password
     mail(to: sending_email, from: email, subject: "New Offer Form Submitted")
